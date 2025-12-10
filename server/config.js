90ae2de7b6629b395991afacf964aa2b62372362
Mingtian Chen <mingtc@seas.upenn.edu>
2025-12-10 15:33:24 -0500

    Create config.js

---
+++ server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rds_host: "dpg-d4sjvh8gjchc738ob9ng-a.virginia-postgres.render.com",
+  rds_port: 5432,
+  rds_user: "aidev_postgres_user",
+  rds_password: "Dh33t5NgfMYS8SZHFGzaVfy7FlVkWk9W",
+  rds_db: "aidev_postgres",
+  server_host: "localhost",
+  server_port: process.env.PORT || 3001
+};
